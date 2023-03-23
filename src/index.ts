823ab6d03c2901e5b84c4a2aae3071d153d4d78e
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2023-03-23 08:22:41 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import 'reflect-metadata';
 import './database/connection';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = process.env.APP_PORT || 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
